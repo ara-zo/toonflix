83e976f9fba49ed6c21735f717478ffe63973898
arazo <jo91kr@gmail.com>
2023-08-20 12:36:51 +0900

    :sparkles: ListView 2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -27,10 +27,17 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView(
-              children: [
-                for (var webtoon in snapshot.data!) Text(webtoon.title),
-              ],
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                print(index);
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(
+                width: 20,
+              ),
             );
           }
           return const Center(
